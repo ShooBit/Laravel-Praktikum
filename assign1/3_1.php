a894084bb8e41726e7535cf84d379490b09b5c32
ShooBit <severin.mengers@googlemail.com>
2022-05-12 10:25:49 +0200

    added 3_1

---
+++ assign1/3_1.php
@@ -0,0 +1,20 @@
+<?php
+$param1 = array("hello","halo","foo");
+$param2 = array("world","steven","bar");
+
+function fun($p1,$p2){
+    if(count($p1) != count($p2)){
+        return "Error input does not have same length.";
+    }elseif(count($p1)==0){
+        return array();
+    }else{
+        $resulting_string = array_shift($p1) . " " . array_shift($p2);
+        $resulted_array = fun($p1, $p2);
+        print_r($resulted_array);
+        array_unshift($resulted_array, $resulting_string);
+        return $resulted_array;
+    }
+}
+$result=fun($param1,$param2);
+print_r($result)
+?>
